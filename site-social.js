2769f5d4389c34b19c0f9864c0049f40e1357dbf
koreshkoreshovich-creator <koreshkoreshovich@gmail.com>
2026-07-27 11:17:04 +0300

    Додати соцмережі та відео станції 30 кВт

---
+++ site-social.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+
+  if (!document.querySelector('link[href="social-video.css"]')) {
+    var stylesheet = document.createElement('link');
+    stylesheet.rel = 'stylesheet';
+    stylesheet.href = 'social-video.css';
+    document.head.appendChild(stylesheet);
+  }
+
+  var footer = document.querySelector('.footer');
+  if (!footer || footer.querySelector('.footer-social')) return;
+
+  var brandColumn = footer.firstElementChild || footer;
+  var social = document.createElement('nav');
+  social.className = 'footer-social';
+  social.setAttribute('aria-label', 'METON у соціальних мережах');
+
+  [
+    ['Instagram', 'IG', 'https://www.instagram.com/meton.com.ua/'],
+    ['TikTok', 'TT', 'https://www.tiktok.com/@m.e.t.o.n'],
+    ['YouTube', 'YT', 'https://www.youtube.com/@Meton_ua']
+  ].forEach(function (network) {
+    var link = document.createElement('a');
+    link.className = 'footer-social-link';
+    link.href = network[2];
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+    link.setAttribute('aria-label', network[0] + ' METON');
+    link.innerHTML = '<span aria-hidden="true">' + network[1] + '</span><strong>' + network[0] + '</strong>';
+    social.appendChild(link);
+  });
+
+  brandColumn.appendChild(social);
+})();
